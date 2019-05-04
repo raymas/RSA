fb6dc820f54ba9932cd8fcb92d2e469ec98318e0
BossaMozza <gauthier.ronne@gmail.com>
2019-05-04 15:36:50 +0200

    Adding checkPrime function

--- prime.py
+++ prime.py
@@ -7,6 +7,7 @@ from multiprocessing import Process
 def generatePrime(size=4096):
     candidate = random.randint(2**(size-1), 2**(size))
 
+
+def checkPrime(number):
     # TODO : check if prime
     # Naive or intelligent method ?
-    
