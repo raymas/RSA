5c2de2390611d17c735d2bcfc9b3b1bbed406376
raymas <cSYvL6@yandex.com>
2019-05-04 17:59:32 +0200

    Test for prime

--- prime.py
+++ prime.py
@@ -3,12 +3,21 @@
 import random
 from multiprocessing import Process
 import time
+import math
 
 import matplotlib.pyplot as plt
 
 def generatePrime(size=4096):
     candidate = random.randint(pow(2, size-1), pow(2, size))
     return candidate
+
+
+
+def checkPrime(number):
+    for i in range(2, int(math.sqrt(number) / 2)):
+        if (number % i) != 0:
+            return False
+    return True
 
 
 def checkPrimeFermat(number):
@@ -28,23 +37,44 @@ def checkPrimeFermat(number):
         return False
 
 
+
+
 def testPrime():
     """Testing prime function"""
-    key_size = []
+
+    start_length = 1
+    end_length = 10
+
+    key_size= []
     time_values = []
 
-    for taille in range(1, 30):
+    print("Starting classical")
+    for taille in range(start_length, end_length):
         key_size.append(taille)
+        start = time.time()
+        isPrime = False
+        while isPrime is False:
+            n = generatePrime(taille)
+            isPrime = checkPrime(n)
+        totalTime = time.time() - start
+        time_values.append(totalTime)
+
+    key_size_fermat = []
+    time_values_fermat = []
+
+    print("Starting Fermat")
+    for taille in range(start_length, end_length):
+        key_size_fermat.append(taille)
         start = time.time()
         isPrime = False
         while isPrime is False:
             n = generatePrime(taille)
             isPrime = checkPrimeFermat(n)
         totalTime = time.time() - start
-        time_values.append(totalTime)
+        time_values_fermat.append(totalTime)
 
-    plt.plot(key_size, time_values)
-    plt.legend("Fermat")
+    plt.plot(key_size, time_values, key_size_fermat, time_values_fermat)
+    plt.legend("Naive sqrt/2 ", "Fermat")
     plt.xlabel("Key size in bits")
     plt.ylabel("Check time in second")
     plt.title("Comparaison of method")
