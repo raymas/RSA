bb7890ae3cfcd7c08e3f89b7e9d1b0e0cc18a55d
BossaMozza <gauthier.ronne@gmail.com>
2019-05-04 17:09:57 +0200

    Adding Fermat algorithm

--- prime.py
+++ prime.py
@@ -11,20 +11,45 @@ def generatePrime(size=4096):
 
 
 def checkPrime(number):
-    for i in range(2, int(number / 2)):
-        if (number % i) == 0:
-            return False
-    return True
+    a = 2
+    if number == 1:
+        return True
+    #print("la valeur de p : ", number)
+    while((a % number) == 0):
+        a += 1
+        #print("la valeur de a: ", a)
+    fermat = pow(a, number) - a
+    #print("la valeur de fermat : ", fermat)
+    #print("divisibilite de fermat : ", fermat % number)
+    if (fermat % number) == 0:
+        return True
+    else:
+        return False
 
 
-for taille in range(10, 60):
-    print("Taille :" + str(taille))
-    start = time.time()
-    isPrime = False
-    while isPrime is False:
-        n = generatePrime(taille)
-        isPrime = checkPrime(n)
-        # print("Check if n is prime : " + str(n) + " result: " + str(isPrime))
+n = 104729
 
-    print("Temps : " + str(time.time() - start) + " secondes")
-    print(n)
+# print(checkPrime(6))
+# print(checkPrime(11))
+# print(checkPrime(5))
+# print(checkPrime(3))
+# print(checkPrime(2))
+# print(checkPrime(97))
+#
+# print(checkPrime(1))
+
+print(checkPrime(n))
+
+
+#
+# for taille in range(10, 60):
+#     print("Taille :" + str(taille))
+#     start = time.time()
+#     isPrime = False
+#     while isPrime is False:
+#         n = generatePrime(taille)
+#         isPrime = checkPrime(n)
+#         # print("Check if n is prime : " + str(n) + " result: " + str(isPrime))
+#
+#     print("Temps : " + str(time.time() - start) + " secondes")
+#     print(n)
