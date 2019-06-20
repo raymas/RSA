84791daa6d8b36b10e6bf65141c7b21746a6c5c4
BossaMozza <gauthier.ronne@gmail.com>
2019-06-20 16:57:07 +0200

    test new function PGCD

--- math_helpers.py
+++ math_helpers.py
@@ -13,6 +13,7 @@ def extended_gcd(x, y):
         return (v, u - (x // y) * v)
 
 
+"""
 def extended_gcd2(x, y):
     (u0, v0, u1, v1) = (1, 0, 0, 1)
     while y:
@@ -20,15 +21,31 @@ def extended_gcd2(x, y):
         (x, y) = (y, r)
         (u0, v0, u1, v1) = (u1, v1, u0-q*u1, v0-q*v1)
         return (u0, v0)
+"""
+
+
+def pgcde(a, b):
+    """ pgcd étendu avec les 2 coefficients de bézout u et v
+        Entrée : a, b entiers
+        Sorties : r = pgcd(a,b) et u, v entiers tels que a*u + b*v = r
+    """
+    r, u, v = a, 1, 0
+    rp, up, vp = b, 0, 1
+    while rp != 0:
+        q = r//rp
+        rs, us, vs = r, u, v
+        r, u, v = rp, up, vp
+        rp, up, vp = (rs - q*rp), (us - q*up), (vs - q*vp)
+    return (r, u, v)
 
 
 if __name__ == "__main__":
     startGCD = "%.20f" % time.time()
-    extended_gcd()
+    print(extended_gcd(120, 23))
     finalTimeGCD = float("%.20f" % time.time()) - float(startGCD)
 
     startGCD2 = "%.20f" % time.time()
-    extended_gcd2()
+    print(extended_gcd2(120, 23))
     finalTimeGCD2 = float("%.20f" % time.time()) - float(startGCD2)
 
-    plt.plot()
+    print(pgcde(120, 23))
