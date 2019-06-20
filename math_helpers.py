45d5f88bf9850befbcbb2fb2e91129efafac3ba8
BossaMozza <gauthier.ronne@gmail.com>
2019-06-20 15:58:12 +0200

    Correcting math_helpers.py

--- math_helpers.py
+++ math_helpers.py
@@ -4,29 +4,31 @@ import time
 import math
 import matplotlib.pyplot as plt
 
+
 def extended_gcd(x, y):
     if y == 0:
         return (1, 0)
     else:
         (u, v) = extended_gcd(y, pow(x, 1, y))
-        return (v, u -  (x // y) * v)
+        return (v, u - (x // y) * v)
+
 
 def extended_gcd2(x, y):
     (u0, v0, u1, v1) = (1, 0, 0, 1)
     while y:
         (q, r) = divmod(x, y)
-        (a, b) = (b, r)
+        (x, y) = (y, x)
         (u0, v0, u1, v1) = (u1, v1, u0-q*u1, v0-q*v1)
         return (u0, v0)
 
+
 if __name__ == "__main__":
-    startGCD    = "%.20f" % time.time()
+    startGCD = "%.20f" % time.time()
     extended_gcd()
     finalTimeGCD = float("%.20f" % time.time()) - float(startGCD)
 
-    startGCD2   = "%.20f" % time.time()
+    startGCD2 = "%.20f" % time.time()
     extended_gcd2()
     finalTimeGCD2 = float("%.20f" % time.time()) - float(startGCD2)
 
     plt.plot()
-
